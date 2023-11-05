aece5742eae0d82476c39f070fa172fc5cf94bdb
Tyuiu.PustobaevDO@outlook.com <Tyuiu.PustobaevDO@outlook.com>
2023-11-05 15:06:58 +0500

    Add project files.

---
+++ Tyuiu.PustobaevDO.Sprint4.Task0.V6.Test/DataSeviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PustobaevDO.Sprint4.Task0.V6.Lib;
+
+namespace Tyuiu.PustobaevDO.Sprint4.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 1, 6, 3, 7, 5, 4, 2, 7, 8, 9 };
+            int res = ds.GetSumOddArrEl(array);
+            int wait = 32;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.PustobaevDO.Sprint4.Task0.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PustobaevDO.Sprint4.Task0.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PustobaevDO.Sprint4.Task0.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44d2d1a8-62ea-4b9e-9659-2c9f0624a705")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
